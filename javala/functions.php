64d4e9adff19663dfd36eec7834d005409ffa76a
Darren King <darrenbrettking@hotmail.com>
2016-07-25 13:24:58 -0700

    Updated naming on funcitons file

--- javala/functions.php
+++ javala/functions.php
@@ -6,12 +6,17 @@ include_once( get_template_directory() . '/lib/init.php' );
 include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
 
 //* Set Localization (do not remove)
-load_child_theme_textdomain( 'beautiful', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'javala' ) );
+load_child_theme_textdomain( 'javala', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'javala' ) );
 
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', __( 'Javala', 'javala' ) );
-define( 'CHILD_THEME_URL', 'http://pixelcurb/themes/javala' );
+define( 'CHILD_THEME_URL', 'http://cmsthemefactory.com/themes/javala/' );
 define( 'CHILD_THEME_VERSION', '1.1' );
+
+add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end' );
+function load_dashicons_front_end() {
+wp_enqueue_style( 'dashicons' );
+}
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption', ) );
@@ -20,10 +25,10 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 add_theme_support( 'genesis-responsive-viewport' );
 
 //* Enqueue scripts and styles
-add_action( 'wp_enqueue_scripts', 'beautiful_enqueue_scripts_styles' );
-function beautiful_enqueue_scripts_styles() {
+add_action( 'wp_enqueue_scripts', 'javala_enqueue_scripts_styles' );
+function javala_enqueue_scripts_styles() {
 
-	wp_enqueue_script( 'beautiful-responsive-menu', get_bloginfo( 'stylesheet_directory' ) . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0' );
+	wp_enqueue_script( 'javala-responsive-menu', get_bloginfo( 'stylesheet_directory' ) . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0' );
 	wp_enqueue_style( 'dashicons' );
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700|Raleway:400,500', array(), CHILD_THEME_VERSION );
 
@@ -31,7 +36,7 @@ function beautiful_enqueue_scripts_styles() {
 
 //* Add support for custom header
 add_theme_support( 'custom-header', array(
-	'default-text-color'     => '000000',
+	'default-text-color'     => 'ffffff',
 	'header-selector'        => '.site-title a',
 	'header-text'            => false,
 	'height'                 => 120,
@@ -59,28 +64,17 @@ unregister_sidebar( 'sidebar-alt' );
 add_action( 'genesis_sidebar_alt', 'genesis_do_sidebar_alt' );
 
 //* Add custom body class to the head
-add_filter( 'body_class', 'beautiful_custom_body_class' );
-function beautiful_custom_body_class( $classes ) {
+add_filter( 'body_class', 'javala_custom_body_class' );
+function javala_custom_body_class( $classes ) {
 
-	$classes[] = 'beautiful';
+	$classes[] = 'javala';
 	return $classes;
 
 }
 
-//* Hook before header widget area above header
-add_action( 'genesis_before_header', 'beautiful_before_header' );
-function beautiful_before_header() {
-
-	genesis_widget_area( 'before-header', array(
-		'before' => '<div class="before-header" class="widget-area"><div class="wrap">',
-		'after'  => '</div></div>',
-	) );
-
-}
-
 //* Hook site header banner after header
-add_action( 'genesis_after_header', 'beautiful_site_header_banner' );
-function beautiful_site_header_banner() {
+add_action( 'genesis_after_header', 'javala_site_header_banner' );
+function javala_site_header_banner() {
 
 	if ( ! get_background_image() )
 		return;
@@ -109,8 +103,8 @@ add_action( 'genesis_after_header', 'genesis_do_subnav', 15 );
 
 
 //* Hook welcome message widget area before content
-add_action( 'genesis_before_loop', 'beautiful_welcome_message' );
-function beautiful_welcome_message() {
+add_action( 'genesis_before_loop', 'javala_welcome_message' );
+function javala_welcome_message() {
 
 	if ( ! is_front_page() || get_query_var( 'paged' ) >= 2 )
 		return;
@@ -122,34 +116,88 @@ function beautiful_welcome_message() {
 
 }
 
+//* Create Specials custom post type
+add_action( 'init', 'javala_specials_post_type' );
+function javala_specials_post_type() {
+
+	register_post_type( 'specials',
+		array(
+			'labels' => array(
+				'name'          => __( 'Specials', 'javala' ),
+				'singular_name' => __( 'Special', 'javala' ),
+			),
+			'has_archive'  => true,
+			'hierarchical' => false,
+			'menu_icon'   => 'dashicons-tag',
+			'public'       => true,
+			'rewrite'      => array( 'slug' => 'specials', 'with_front' => false ),
+			'supports'     => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'revisions', 'page-attributes', 'genesis-seo', 'genesis-cpt-archives-settings' ),
+			'taxonomies'   => array( 'specials-type' ),
+
+		)
+	);
+
+}
+
+//* Add Specials Type Taxonomy to columns
+add_filter( 'manage_taxonomies_for_specials_columns', 'javala_specials_columns' );
+function javala_specials_columns( $taxonomies ) {
+
+    $taxonomies[] = 'specials-type';
+    return $taxonomies;
+
+}
+
+//* Change the number of specials items to be displayed (props Bill Erickson)
+add_action( 'pre_get_posts', 'javala_specials_items' );
+function javala_specials_items( $query ) {
+
+	if( $query->is_main_query() && !is_admin() && is_post_type_archive( 'specials' ) ) {
+		$query->set( 'posts_per_page', '12' );
+	}
+
+}
+
+//* Customize Specials post info and post meta
+add_filter( 'genesis_post_info', 'javala_specials_post_info_meta' );
+add_filter( 'genesis_post_meta', 'javala_specials_post_info_meta' );
+function javala_specials_post_info_meta( $output ) {
+
+     if( 'specials' == get_post_type() )
+        return '';
+
+    return $output;
+
+}
+
+//* Remove featured image support from Specials custom post type
+function remove_featured_image_specials() {
+	remove_theme_support( 'post-thumbnails', array( 'specials' ) );
+}
+add_action( 'admin_menu', 'remove_featured_image_specials' );
+
 //* Modify the WordPress read more link
-add_filter( 'the_content_more_link', 'beautiful_read_more' );
-function beautiful_read_more() {
-
-	return '<a class="more-link" href="' . get_permalink() . '">' . __( 'Continue Reading', 'beautiful' ) . '</a>';
-
+add_filter( 'the_content_more_link', 'javala_read_more' );
+function javala_read_more() {
+	return '<a class="more-link" href="' . get_permalink() . '">' . __( 'Continue Reading', 'javala' ) . '</a>';
 }
 
 //* Modify the content limit read more link
-add_action( 'genesis_before_loop', 'beautiful_more' );
-function beautiful_more() {
-
-	add_filter( 'get_the_content_more_link', 'beautiful_read_more' );
-
+add_action( 'genesis_before_loop', 'javala_more' );
+function javala_more() {
+	add_filter( 'get_the_content_more_link', 'javala_read_more' );
 }
 
-add_action( 'genesis_after_loop', 'beautiful_remove_more' );
-function beautiful_remove_more() {
-
-	remove_filter( 'get_the_content_more_link', 'beautiful_read_more' );
-
+add_action( 'genesis_after_loop', 'javala_remove_more' );
+function javala_remove_more() {
+	remove_filter( 'get_the_content_more_link', 'javala_read_more' );
 }
 
 //* Remove entry meta in entry footer
-add_action( 'genesis_before_entry', 'beautiful_remove_entry_meta' );
-function beautiful_remove_entry_meta() {
+add_action( 'genesis_before_entry', 'javala_remove_entry_meta' );
+function javala_remove_entry_meta() {
 
-	//* Remove if not single post
+//* Remove if not single post
 	if ( ! is_single() ) {
 		remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
 		remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
@@ -159,55 +207,23 @@ function beautiful_remove_entry_meta() {
 }
 
 //* Modify the size of the Gravatar in the author box
-add_filter( 'genesis_author_box_gravatar_size', 'beautiful_author_box_gravatar' );
-function beautiful_author_box_gravatar( $size ) {
-
+add_filter( 'genesis_author_box_gravatar_size', 'javala_author_box_gravatar' );
+function javala_author_box_gravatar( $size ) {
 	return 180;
-
 }
 
 //* Modify the size of the Gravatar in the entry comments
-add_filter( 'genesis_comment_list_args', 'beautiful_comments_gravatar' );
-function beautiful_comments_gravatar( $args ) {
+add_filter( 'genesis_comment_list_args', 'javala_comments_gravatar' );
+function javala_comments_gravatar( $args ) {
 	$args['avatar_size'] = 100;
 	return $args;
 }
 
-//* Hook split sidebar and bottom sidebar widget areas below primary sidebar
-add_action( 'genesis_after_sidebar_widget_area', 'beautiful_extra_sidebars' );
-function beautiful_extra_sidebars() {
-
-	if ( is_active_sidebar( 'split-sidebar-left' ) || is_active_sidebar( 'split-sidebar-right' ) ) {
-
-		echo '<div class="split-sidebars">';
-
-			genesis_widget_area( 'split-sidebar-left', array(
-				'before' => '<div class="split-sidebar-left" class="widget-area">',
-				'after'  => '</div>',
-			) );
-			genesis_widget_area( 'split-sidebar-right', array(
-				'before' => '<div class="split-sidebar-right" class="widget-area">',
-				'after'  => '</div>',
-			) );
-
-		echo '</div>';
-
-	}
-
-	genesis_widget_area( 'bottom-sidebar', array(
-		'before' => '<div class="bottom-sidebar" class="widget-area">',
-		'after'  => '</div>',
-	) );
-
-}
-
 //* Remove comment form allowed tags
-add_filter( 'comment_form_defaults', 'beautiful_remove_comment_form_allowed_tags' );
-function beautiful_remove_comment_form_allowed_tags( $defaults ) {
-
+add_filter( 'comment_form_defaults', 'javala_remove_comment_form_allowed_tags' );
+function javala_remove_comment_form_allowed_tags( $defaults ) {
 	$defaults['comment_notes_after'] = '';
 	return $defaults;
-
 }
 
 //* Add new image size
@@ -215,50 +231,28 @@ add_image_size( 'promotion', 382, 282, TRUE );
 
 //* Register widget areas
 genesis_register_sidebar( array(
-	'id'          => 'before-header',
-	'name'        => __( 'Before Header', 'beautiful' ),
-	'description' => __( 'This is the before header widget area.', 'beautiful' ),
-) );
-genesis_register_sidebar( array(
 	'id'          => 'homepage-slideshow',
-	'name'        => __( 'Homepage Slideshow', 'beautiful' ),
-	'description' => __( 'This is the welmain homepage slideshow area.', 'beautiful' ),
+	'name'        => __( 'Homepage Slideshow', 'javala' ),
+	'description' => __( 'This is the main homepage slideshow area.', 'javala' ),
 ) );
 genesis_register_sidebar( array(
 	'id'          => 'promotions',
-	'name'        => __( 'Promotions', 'beautiful' ),
-	'description' => __( 'This is the promotions widget area.', 'beautiful' ),
+	'name'        => __( 'Promotions', 'javala' ),
+	'description' => __( 'This is the promotions widget area.', 'javala' ),
 ) );
-genesis_register_sidebar( array(
-	'id'          => 'split-sidebar-left',
-	'name'        => __( 'Split Sidebar Left', 'beautiful' ),
-	'description' => __( 'This is the left split sidebar widget area.', 'beautiful' ),
-) );
-genesis_register_sidebar( array(
-	'id'          => 'split-sidebar-right',
-	'name'        => __( 'Split Sidebar Right', 'beautiful' ),
-	'description' => __( 'This is the right split sidebar widget area.', 'beautiful' ),
-) );
-genesis_register_sidebar( array(
-	'id'          => 'bottom-sidebar',
-	'name'        => __( 'Bottom Sidebar', 'beautiful' ),
-	'description' => __( 'This is the bottom sidebar widget area.', 'beautiful' ),
-) );
+
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+add_action( 'genesis_footer', 'dbk_footer' );
+function dbk_footer() {
+    ?>
+    <p><a href="http://cmsthemefactory.com">WordPress Coffee Shop Theme - &copy; 2016 by CMS Themefactory</a>. All Rights Reserved.</p>
+    <?php
+}
 
 add_theme_support( 'genesis-connect-woocommerce' );
 
 add_filter( 'woocommerce_product_tabs', 'sb_woo_remove_reviews_tab', 98);
 function sb_woo_remove_reviews_tab($tabs) {
-
  unset($tabs['reviews']);
-
  return $tabs;
-}
-
-remove_action( 'genesis_footer', 'genesis_do_footer' );
-add_action( 'genesis_footer', 'wpvkp_footer' );
-function wpvkp_footer() {
-    ?>
-    <p><a href="http://cmsthemefactory.com">WordPress Coffee Shop Theme - &copy; 2016 by CMS Themefactory</a>. All Rights Reserved.</p>
-    <?php
 }
